c2c8dfcab65e619b75d93b97819ff3a76f44ad73
Phu Truong Van <phutv@asiantech.vn>
2016-06-29 11:48:13 +0700

    Firstly, Initialing skeleton for Final FE project.

--- app/js/app.js
+++ app/js/app.js
@@ -1,32 +1,21 @@
 'use strict';
 
 // App module
-var wfeApp = angular.module('wfeApp', [
+var myApp = angular.module('myApp', [
   'ngRoute', // All routing, navigating in the app goes here
-  'wfeControllers', // Our beloved controller module collection
-  'wfeServices', // Call our custom services
+  'myControllers' // Our beloved controller module collection
 ]);
 
-wfeApp.config(['$routeProvider', function ($routeProvider) {
+myApp.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
-    .when('/people', {
-      templateUrl: 'view/people.html',
-      controller: 'PeopleListCtrl',
-    })
-    // :codeName as going to stand for person identity
-    .when('/person/:codeName', {
-      templateUrl: 'view/person.html',
-      controller: 'PersonDetailCtrl',
+    .when('/home', {
+      templateUrl: 'view/home.html',
+      controller: 'HomeCtrl',
     })
     // any link goes beyond defined routes will be redirected to /people route.
     .otherwise({
-      redirectTo: '/people',
+      redirectTo: '/home',
     });
 }]);
 
-wfeApp.controller('HeaderPartialCtrl', ['$scope', function ($scope) {
-}]);
 
-wfeApp.controller('FooterPartialCtrl', ['$scope', function ($scope) {
-  // Do something...
-}]);
