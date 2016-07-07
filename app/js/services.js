b4c5a5cee661d6c95e07ed034cb869696e0fa24f
Phu Truong Van <phutv@asiantech.vn>
2016-07-08 00:01:53 +0700

    fix typo and convention v1.0

--- app/js/services.js
+++ app/js/services.js
@@ -1,8 +1,7 @@
 'use strict';
-
 var myServices = angular.module('myServices', ['ngResource']);
 
-myServices.factory('FeedService',['$http',function($http){
+myServices.factory('FeedService',['$http',function ($http) {
 	return {
 		parseFeed : function(url){
 			return $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
